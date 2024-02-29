1ae05df322ca86b5f890ef1d813d25668e945c1e
melektron <matteo@elektron.work>
2024-02-29 12:31:36 +0100

    tested tcp keepalive

---
+++ protocol_test.py
@@ -0,0 +1,67 @@
+
+"""
+protocol_test.py
+29. February 2024
+
+test implementation of sockets for protocol development
+
+Author:
+melektron
+"""
+
+import asyncio
+import socket
+
+async def handle_echo(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    # configure keepalive
+    # https://stackoverflow.com/questions/75326508/set-socket-options-on-python-asyncio-streams-api
+    sock: socket.socket = writer.get_extra_info("socket")
+    print(f"SO_KEEPALIVE={sock.getsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE)}")
+    print(f"TCP_KEEPIDLE={sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE)}")
+    print(f"TCP_KEEPINTVL={sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL)}")
+    print(f"TCP_KEEPCNT={sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT)}")
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, True)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 2)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 2)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 2)
+    print(f"SO_KEEPALIVE={sock.getsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE)}")
+    print(f"TCP_KEEPIDLE={sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE)}")
+    print(f"TCP_KEEPINTVL={sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL)}")
+    print(f"TCP_KEEPCNT={sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT)}")
+
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    await asyncio.sleep(20);
+
+    print("Close the connection")
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main() -> int:
+    server = await asyncio.start_server(handle_echo, '0.0.0.0', 12345)
+    
+
+    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print(f'Serving on {addrs}')
+
+    async with server:
+        try:
+            await server.serve_forever()
+        except asyncio.CancelledError:
+            print("anceled")
+            pass
+    
+    return 0;
+
+
+if __name__ == "__main__":
+    exit(asyncio.run(main()))
